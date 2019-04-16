d56d1e4041240f8811035c3cca1fe2b888db43a8
Andrievska Nastia <42992140+Andrievska-Nastia@users.noreply.github.com>
2019-04-16 15:18:11 +0300

    lab 2-2

---
+++ 2-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int arr1;
+	int a;
+	bool found = false;
+	cin >> arr1;
+	vector<int> myVector(arr1);
+	for (int i = 0; i < arr1; i++)
+	{
+		cin >> myVector[i];
+	}
+	a = arr1;
+	for (int t = 0; t < arr1; t++)
+	{
+		a--;
+		for (int i = 0; i < a; i++)
+		{
+			if (myVector[a] < myVector[i])
+			{
+				swap(myVector[a], myVector[i]);
+			}
+		}
+	}
+
+	for (int i = 0; i < arr1; i++)
+	{
+		cout << myVector[i];
+		cout << " ";
+	}
+}
